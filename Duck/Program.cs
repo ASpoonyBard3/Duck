dac199e4e17764207eb7051dc34c05a143a34740
ASpoonyBard3 <Samsonf21@gmail.com>
2017-12-30 16:37:19 +0000

    Duck Exercise, split out class from var assignment.

--- Duck/Program.cs
+++ Duck/Program.cs
@@ -10,6 +10,25 @@ namespace Duck
     {
         static void Main(string[] args)
         {
+            var quack = new Duck("Sir Quackalot", "Mallard", 100, 17);
         }
     }
+    public class Duck
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public int WeightInGrams { get; }
+        public int AgeInMonths { get; }
+
+        public Duck(string name, string type, int weightInGrams, int ageInMonths)
+        {
+            Name = name;
+            Type = type;
+            WeightInGrams = weightInGrams;
+            AgeInMonths = ageInMonths;
+        }
+    }
+
+
+
 }
